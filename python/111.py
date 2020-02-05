bdc2714fa639fb40925c66ef4b5fe55a54e60fc5
tsupei <tsupei0527@gmail.com>
2020-02-06 01:24:57 +0800

    📜 Others: Problem 111 improved (Python)
    (Easy) Minimum Depth of Binary Tree
    
    使用一個Stack或者Queue存節點，若是無左右子樹表示結束，比較深度，反之則加到queue中繼續計算
    
    Speed: 90.73%
    Space: 82.05%
    
    https://leetcode.com/problems/minimum-depth-of-binary-tree/

--- python/111.py
+++ python/111.py
@@ -12,12 +12,31 @@ class Solution(object):
         :rtype: int
         """
 
+        # if not root:
+        #     return 0
+        # if not root.left and not root.right:
+        #     return 1
+        # if not root.left and root.right:
+        #     return self.minDepth(root.right) + 1
+        # if not root.right and root.left:
+        #     return self.minDepth(root.left)
+        # return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+
+        # Solution 2
         if not root:
             return 0
-        if not root.left and not root.right:
-            return 1
-        if not root.left and root.right:
-            return self.minDepth(root.right) + 1
-        if not root.right and root.left:
-            return self.minDepth(root.left)
-        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        minima = -1
+        q = [(root, 1)]
+        while q:
+            cur, level = q.pop()
+            if not cur.left and not cur.right:
+                if minima == -1:
+                    minima = level
+                else:
+                    minima = min(minima, level)
+            if cur.left:
+                q.append((cur.left, level+1))
+            if cur.right:
+                q.append((cur.right, level+1))
+        return minima
+
