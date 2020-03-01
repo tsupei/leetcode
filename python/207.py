f575dc6e803715ab437da3b1eb7963d8637c6547
tsupei <tsupei0527@gmail.com>
2020-03-01 16:44:24 +0800

    📜 Others: Problem 207 improved (Python)
    (Medium) Course Schedule
    
    Greedy，每次都把能修的課全部修完，看最後能不能修完所有課
    一但有一輪沒有修到課，就跳出
    最差的情況同樣是O(n^2)
    
    Speed: 12.5%
    Space: 72.88%
    
    Wait Improvement
    
    https://leetcode.com/problems/course-schedule

--- python/207.py
+++ python/207.py
@@ -1,42 +1,42 @@
 class Solution(object):
-    def walk(self, v, g, s, c):
-        # v: record visited points
-        # g: graph
-        # s: start point
-        # c: current point
-        v[c] = True
-        print(s,c)
-        if g[c]:
-            for p in g[c]:
-                if not v[p]:
-                    if not self.walk(v, g, s, p):
-                        return False
-                else:
-                    if p == s:
-                        return False
-        return True
-
     def canFinish(self, numCourses, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
+        # Greedy
         graph = {}
         for i in range(numCourses):
             graph[i] = []
-        for target, pre in prerequisites:
-            graph[target].append(pre)
-        # If a loop exist, then invalid
-        for i in range(numCourses):
-            v = [False] * numCourses
-            if not self.walk(v, graph, i, i):
-                return False
+        for to_node, from_node in prerequisites:
+            graph[to_node].append(from_node)
+            
+        cands = []
+        for k, v in graph.items():
+            if not v:
+                cands.append(k)
+        
+        taken = [0] * numCourses
+        while cands:
+            for cand in cands:
+                taken[cand] = 1
+            cands = []
+            for k, v in graph.items():
+                if taken[k] == 0:
+                    for prerequisite in v:
+                        if taken[prerequisite] != 1:
+                            break
+                    else:
+                        cands.append(k)
+        if sum(taken) != numCourses:
+            return False
         return True
+
 
 if __name__ == "__main__":
     sol = Solution()
-    numCourses = 4
-    prerequisites = [[0,1],[3,1],[1,3],[3,2]]
+    numCourses = 3
+    prerequisites = [[1,0],[1,2],[0,1]] 
     res = sol.canFinish(numCourses, prerequisites)
     print(res)
